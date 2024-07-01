88fd0f6f5618843d9651f73827e7b4b373d90c26
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 13:27:07 +0300

    relative absolute

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,18 @@ import Header from './components/Header.vue'
   <div class="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14">
     <Header />
     <div>
-      <img src="/like-1.svg" alt="Like 1" />
-      <img src="/sneakers/sneakers-1.jpg" alt="Like 1" />
-      <p>Nike Blazer Mid Suede</p>
-      <div class="flex justify-between">
-        <div>
-          <span>2250TL</span>
+      <div
+        class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer hover:translate-y-2"
+      >
+        <img src="/like-1.svg" alt="Like 1" class="absolute top-0 left-0" />
+        <img src="/sneakers/sneakers-1.jpg" alt="Like 1" />
+        <p>Nike Blazer Mid Suede</p>
+        <div class="flex justify-between">
+          <div class="flex flex-col">
+            <span>2250TL</span>
+          </div>
+          <img src="/plus.svg" alt="Plus" />
         </div>
-        <img src="/plus.svg" alt="Plus" />
       </div>
     </div>
   </div>
