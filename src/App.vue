67ecafc7b329ad4ed0e1bfeed5ba9f033e2f78c4
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 14:41:43 +0300

    drawer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Card from './components/Card.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
