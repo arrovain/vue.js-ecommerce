ca39f3045b24b4cbfaa64d0ba42ee344797543fc
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 13:31:51 +0300

    card as a components

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <script setup>
 import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
   <div class="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14">
     <Header />
     <div>
-      <div
-        class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer transition hover:translate-y-2 hover:shadow-xl"
-      >
-        <img src="/like-1.svg" alt="Like 1" class="absolute top-0 left-0" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Like 1" />
-        <p>Nike Blazer Mid Suede</p>
-        <div class="flex justify-between mt-5">
-          <div class="flex flex-col">
-            <span>2250TL</span>
-          </div>
-          <img src="/plus.svg" alt="Plus" />
-        </div>
-      </div>
+      <Card />
     </div>
   </div>
 </template>
