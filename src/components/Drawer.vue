d4b77a073325cd2ab6464fba06933ae90ca77359
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 14:45:08 +0300

    design

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opactiy-70">
+    <div class="bg-white w-96 h-full fixed right-0 z-20 top-0 p-8">
+      <h2></h2>
+    </div>
+  </div>
+</template>
