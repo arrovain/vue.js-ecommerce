22eec72fae5d3204856b67b2a2595ad456d90e83
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 13:48:35 +0300

    CardList as a component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
 import Header from './components/Header.vue'
-import Card from './components/Card.vue'
+import CardList from './components/CardList.vue'
 </script>
 
 <template>
   <div class="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14">
     <Header />
-    <h2 text-3xl fonst-bold mb-10>SNEAKERLAR</h2>
-    <div class="grid grid-cols-4">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <h2 class="text-3xl font-bold mb-8">SNEAKERLAR</h2>
+    <CardList />
   </div>
 </template>
