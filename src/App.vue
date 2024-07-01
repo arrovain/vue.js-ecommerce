6b37c8197f07313b668aef13edb05a6cc8eca440
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 11:51:07 +0300

    heart

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,8 @@
       </div>
 
       <ul>
-        <li><img src="/cart.svg" alt="cart" /> <b>1205 TL</b></li>
-        <li></li>
+        <li><img src="/cart.svg" alt="Cart" /> <b>1205 TL</b></li>
+        <li><img src="/heart.svg" alt="Cart" /> <b></b></li>
         <li></li>
       </ul>
     </header>
