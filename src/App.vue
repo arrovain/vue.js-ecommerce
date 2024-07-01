53b9d0612d70525b6e0f4a98c516173ca2a07296
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 12:57:31 +0300

    style

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
 
 <template>
   <div className="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14 ">
-    <header>
-      <div>
-        <img src="/logo.png" alt="logo" />
+    <header class="flex justify-between border-b border-slate-300 px-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="logo" class="w-10" />
         <div>
-          <h2>Vue Sneakers</h2>
+          <h2>İcme Sneakers</h2>
           <p>Best seller sneakers</p>
         </div>
       </div>
 
-      <ul>
-        <li><img src="/cart.svg" alt="Cart" /> <b>1205 TL</b></li>
-        <li><img src="/heart.svg" alt="Cart" /> <b></b></li>
-        <li><img src="/profile.svg" alt="Cart" /> <b></b>/li></li>
+      <ul class="">
+        <li><img src="/cart.svg" alt="Cart" /> <b>sepet</b></li>
+        <li><img src="/heart.svg" alt="Cart" /> <b>favoriler</b></li>
+        <li><img src="/profile.svg" alt="Cart" /> <b>hesabım</b></li>
       </ul>
     </header>
 
