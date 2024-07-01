1e57852e26649a0f6fe449d6a589c646608c6499
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 15:15:52 +0300

    refs and totalPrice and VatPrice functions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
+import { ref, provide, watch, computed } from 'vue'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Card from './components/Card.vue'
 import Drawer from './components/Drawer.vue'
+const cart = ref([])
+const drawerOpen = ref(false)
+
+const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
+const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
 </script>
 
 <template>
   <div class="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14">
     <Header />
     <h2 class="text-3xl font-bold mb-8">SNEAKERLAR</h2>
-    <CardList />
   </div>
 </template>
