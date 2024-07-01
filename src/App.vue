56b41d306c680dea66dfab6680e832041a82d27d
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 11:46:10 +0300

    logo

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,22 @@
 
 <template>
   <div className="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14 ">
+    <header>
+      <div>
+        <img src="/logo.png" alt="logo" />
+        <div>
+          <h2>Vue Sneakers</h2>
+          <p>Best seller sneakers</p>
+        </div>
+      </div>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </header>
+
     <h1>blog</h1>
   </div>
 </template>
