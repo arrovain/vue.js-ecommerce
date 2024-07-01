04e459d9a71f6a6fb822711d356b4902924463ac
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 13:29:00 +0300

    transition and hover

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@ import Header from './components/Header.vue'
     <Header />
     <div>
       <div
-        class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer hover:translate-y-2"
+        class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer transition hover:translate-y-2 hover:shadow-xl"
       >
         <img src="/like-1.svg" alt="Like 1" class="absolute top-0 left-0" />
         <img src="/sneakers/sneakers-1.jpg" alt="Like 1" />
         <p>Nike Blazer Mid Suede</p>
-        <div class="flex justify-between">
+        <div class="flex justify-between mt-5">
           <div class="flex flex-col">
             <span>2250TL</span>
           </div>
