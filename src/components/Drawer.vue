8d75d9a52d8b92ca0c0b82333355cd9d470b4e48
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 15:39:23 +0300

    InfoBlock

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,10 @@
 <script>
-import DrawerHead from './DrawerHead.vue'
+import axios from 'axios'
 import { ref, computed, inject } from 'vue'
+
+import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
+import InfoBlock from './InfoBlock.vue'
 
 const props = defineProps({
   totalPrice: Number,
@@ -32,15 +36,53 @@ const createOrder = async () => {
 }
 
 const cartIsEmpty = computed(() => cart.value.length === 0)
-const buttonDisabled = computed(() => isCreating.value || cartIsEmpty.value)
+const buttonDisabled = computed(() = isCreating.value || cartIsEmpty.value)
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opactiy-70">
-    <div class="bg-white w-96 h-full fixed right-0 z-20 top-0 p-8">
-      <DrawerHead />
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
 
-      <h2 class="text-xl font-bold"></h2>
+    <div v-if="!totalPrice || orderId" class="flex h-full items-center">
+      <InfoBlock
+        v-if="!totalPrice && !orderId"
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+        image-url="/package-icon.png"
+      />
+      <InfoBlock
+        v-if="orderId"
+        title="Заказ оформлен!"
+        :description="`Ваш заказ #${orderId} скоро будет передан курьерской доставке`"
+        image-url="/order-success-icon.png"
+      />
+    </div>
+
+    <div v-else>
+      <CartItemList />
+
+      <div class="flex flex-col gap-4 mt-7">
+        <div class="flex gap-2">
+          <span>Toplam:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} ₽</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span>Vergi 5%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} ₽</b>
+        </div>
+
+        <button
+          :disabled="buttonDisabled"
+          @click="createOrder"
+          class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+        >
+          Çıkış yap
+        </button>
+      </div>
     </div>
   </div>
 </template>
