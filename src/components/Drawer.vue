080310be387a6311b1032cc6f4ef0b88e80810e3
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 14:47:52 +0300

    svg

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opactiy-70">
     <div class="bg-white w-96 h-full fixed right-0 z-20 top-0 p-8">
-      <h2></h2>
+      <div>
+        <svg
+          width="16"
+          height="14"
+          viewBox="0 0 16 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 7H14.7143"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M8.71436 1L14.7144 7L8.71436 13"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+      <h2 class="text-xl font-bold"></h2>
     </div>
   </div>
 </template>
