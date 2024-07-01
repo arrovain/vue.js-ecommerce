b699a232860c2c738cdc2f5296fe30e6850d4524
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 14:36:49 +0300

    isAdded

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,7 +17,12 @@ defineProps({
       <div
         class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer transition hover:translate-y-2 hover:shadow-xl"
       >
-        <img src="/like-1.svg" alt="Like 1" class="absolute top-0 left-0" @click="onClickAdd" />
+        <img
+          src="/like-1.svg"
+          alt="Like 1"
+          class="absolute top-0 left-0"
+          @click="onClickFavorite"
+        />
         <img :src="imageUrl" alt="Like 1" />
         <p>{{ title }}</p>
         <div class="flex justify-between mt-5">
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,10 +1,21 @@
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('eklendi')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card title="Nike Blazer Mid" imageUrl="/sneakers/sneakers-1.jpg" :price="2850" />
+    <Card
+      title="Nike Blazer Mid"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="2850"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+      :isAdded="true"
+    />
     <Card />
     <Card />
     <Card />
