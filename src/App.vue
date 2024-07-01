6871375d5d53bad0d1a8abc2d67764bae506d8ce
Mahmut İÇME <arrovain@gmail.com>
2024-07-01 13:20:16 +0300

    plus

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div className="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14 ">
+  <div class="bg-white rounded-xl h-screen w-4/5 m-auto shadow-xl mt-14">
     <Header />
     <div>
       <img src="/like-1.svg" alt="Like 1" />
       <img src="/sneakers/sneakers-1.jpg" alt="Like 1" />
+      <p>Nike Blazer Mid Suede</p>
+      <div class="flex justify-between">
+        <div>
+          <span>2250TL</span>
+        </div>
+        <img src="/plus.svg" alt="Plus" />
+      </div>
     </div>
   </div>
 </template>
